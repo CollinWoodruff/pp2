1cffca507e7654ec34bf8847bcaed673fd2fe7a1
Collin Woodruff <Cwoodruff@mail.greenriver.edu>
2020-04-17 16:21:24 -0400

    Step 1 finished

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Collin Woodruff
+ * Date: 4/17/2020
+ * Time: 1:11 PM
+ * http://cwoodruff.greenriverdev.com/328X2/pp2/
+ */
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+echo "<p>step 1</p>
+<h1>PHP Array Practice</h1>";
